520f1f58bf1d06085eefcdc3948f3bd6164fc4f2
darda <dardali@mail.ru>
2023-04-18 15:54:15 +0300

    a

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
                             default="django.db.backends.postgresql"),
         'NAME': os.getenv('DB_NAME', default="postgres"),
         'USER': os.getenv('POSTGRES_USER', default="postgres"),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default="postgres"),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default="Wot95region"),
         'HOST': os.getenv('DB_HOST', default="db"),
         'PORT': os.getenv('DB_PORT', default="5432")
     }
